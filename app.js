0aad874ea257080ccf569ad016345850d77b2f29
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-10-20 15:40:16 +0530

    done

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express=require("express");
 const app=express();
+const http=require("http");
+const socketio=require("socket.io");
+const server =http.createServer(app);
+const io=socketio(server);
+app.set("view engine","ejs");
+app.set(express.static(path.join(__dirname,"public")));
+
+
 app.get("/",function(req,res){
       res.send("HEY")
 });
-app.listen(3000);
+server.listen(3000);
